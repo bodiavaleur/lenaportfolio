ba2b08ce9a55836afcd0a17b591a8badb56fd613
pupyrkapains <lena.pendtyk@gmail.com>
2021-06-28 23:17:57 +0300

    featured-works

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Header from "./components/Header";
 import MyInfo from "./components/MyInfo";
 import RecentPosts from "./components/RecentPosts";
+import FeaturedWorks from "./components/FeaturedWorks";
 
 export function App() {
   return (
@@ -9,6 +10,7 @@ export function App() {
       <Header />
       <MyInfo />
       <RecentPosts />
+      <FeaturedWorks />
     </>
   );
 }
